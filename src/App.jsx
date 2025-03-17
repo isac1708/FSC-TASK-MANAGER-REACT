5fb7bbbe36c13e33ea4a574332f1aa245c3446d8
isac1708 <isac.figueredo@gmail.com>
2025-03-17 12:17:28 -0300

    chore: add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-function app() {
+
+
+
+function App() {
   return (
     <div>
-      <h1>My React App</h1>
-      <p>Welcome to my React app</p>
+      <h1 className = "p-6">My React App</h1>
+      
     </div>
   );
 } 
-export default app;
+export default App;
