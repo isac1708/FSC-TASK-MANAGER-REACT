f3f10f6a3cdeb3810f066c6496300a1760fe8eb1
isac1708 <isac.figueredo@gmail.com>
2025-03-24 23:59:44 -0300

    feat: Adding dynamism to the task icons and buttons.

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,19 +1,32 @@
+import CheckIcon from "../assets/icons/check.svg?react";
+import LoaderIcon from "../assets/icons/LoaderIcon";
+import DetailsIcon from "../assets/icons/details.svg?react";
+
+
 const TaskItem = ({task}) => {
     const getStatusClasses=()=>{
         if(task.status === "done"){
-            return "bg-[#00ADB5] bg-opacity-10  text-[#00ADB5]";
+            return "bg-[#00ADB5]   text-[#00ADB5]";
         }
         if(task.status === "in_progress"){
-            return "bg-[#FFAA04] bg-opacity-10 text-[#FFAA04]";
+            return "bg-[#FFAA04]  text-[#FFAA04]";
         }
         if(task.status === "not_started"){
             return "bg-[#35383e] bg-opacity-10 text-[#35383e]";
         }
         
     };
-
-   return( <div className={`flex items-center gap-2  px-4 py-3 rounded-xl text-sm ${getStatusClasses()}`} >
+    console.log("Task status:", task.status); // Adicionado console.log aqui
+   return( <div className={`flex items-center gap-2 bg-opacity-10 px-4 py-3 rounded-xl text-sm justify-between ${getStatusClasses()}`} >
+    <div className="flex items-center gap-2">
+    <label className={`relative flex h-7 w-7 cursor-pointer items-center  justify-center rounded-lg ${getStatusClasses()}`}>
+        <input type ="checkbox" checked={task.status==="done"}  className="absolute h-full w-full curso-pointer opacity-0"/>
+        {task.status==="done" && <CheckIcon/>}
+        {task.status==='in_progress' && <LoaderIcon className= "animate-spin"/>}
+    </label>
     {task.title}
+    </div>
+    <a  href="#" className="hove-opacity-75 transition "> <DetailsIcon/> </a>
 </div>);
 }
 export default TaskItem;
