f01029dd7a1b7183849e4da5fc24c6d8adf5c8e2
isac1708 <isac.figueredo@gmail.com>
2025-04-15 23:56:52 -0300

    feat: update spacing between sidebar  and tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -38,7 +38,7 @@ const handleTaskCheckBoxClick=( taskId) => {
 
 
   return (
-    <div className="w-full px-8 py-16">
+    <div className="w-full px-8 py-16 space-y-6">
       <div className="flex w-full justify-between">
         <div>
           <span className="text-xs font-semibold text-[#00ADB5]">
