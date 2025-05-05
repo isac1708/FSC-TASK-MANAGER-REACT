586aa45738821e72be77fa55a62edc504900b5a8
isac1708 <isac.figueredo@gmail.com>
2025-05-05 19:51:50 -0300

    feat: ensure AddTaskDialog can be closed

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,6 +1,8 @@
 import { createPortal } from "react-dom";
+import Input from "./input";
+import Button from "./Button";
 
-const AddTaskDialog = ({isOpen}) => {
+const AddTaskDialog = ({isOpen,handleClose }) => {
 
     if(!isOpen) return null;
 
@@ -10,9 +12,22 @@ return createPortal(
         <h2 className="text-[#35383E] font-semibold text-xl">
             Adicionar nova tarefa
         </h2>
-        <p className="text-[#9A9C9F] text-sm font-normal mt-1">
+        <p className="text-[#9A9C9F] text-sm font-normal mt-1 mb-4">
             Adicione uma nova tarefa para a sua lista de tarefas
         </p>
+        <div className="flex flex-col space-y-4 w-[336px]">
+            <Input id="title" label="Título" placeholder="Insira o titulo da tarefa"/>
+            <Input id="hour" label="Horário" placeholder="Horário"/>
+            <Input id="description" label="Descrição" placeholder="Descreva a tarefa"/>
+            <div className="flex gap-3 ">
+                <Button className="w-full" size="large" variant="secondary" onClick={handleClose}>
+                    Cancelar
+                </Button>
+                <Button className="w-full" size="large">
+                    Salvar
+                </Button>
+            </div>
+        </div>
       </div>
     </div>,
     document.body
